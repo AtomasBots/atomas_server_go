5edba1f1b4cfc8dea38ccdf27a9d033e361a0306
ordon <ordon1313@gmail.com>
2015-12-12 00:16:59 +0100

    full game dto - json fix

--- atomas/dto.go
+++ atomas/dto.go
@@ -4,6 +4,6 @@ type GameDTO struct {
 	Id    string `json:"id"`
 	Board []int `json:"board"`
 	Next  int `json:"next"`
-	Round int `json:"next"`
+	Round int `json:"round"`
 	Score int `json:"score"`
 }
