8c74afd20e0981e219977690cfb31d8879194e08
ordon <ordon1313@gmail.com>
2015-12-12 10:53:15 +0100

    move endpoint

--- main/main.go
+++ main/main.go
@@ -23,6 +23,7 @@ func main() {
 	games := map[string]atomas.GameDTO{}
 	http.HandleFunc("/new_game", atomas.CreateCreateGameHandler(games, nextUUID, elementGenerator))
 	http.HandleFunc("/game/", atomas.CreateGetGameHandler(games))
+	http.HandleFunc("/move/", atomas.CreateMoveHandler(games, elementGenerator))
 	port := os.Args[1]
 	log.Fatal(http.ListenAndServe(port, nil))
 }
