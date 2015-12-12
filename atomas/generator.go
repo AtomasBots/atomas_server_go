93766fb8b4d5d288ad8af7b5d1887f3fce292e6f
ordon <ordon1313@gmail.com>
2015-12-12 23:54:56 +0100

    random element generator with small logic

--- atomas/generator.go
+++ atomas/generator.go
@@ -2,6 +2,10 @@ package atomas
 
 func CreateElementGenerator(rand func() int) func(int) int {
 	return func(round int) int {
-		return rand() % 3 + 1
+		if (round == 0) {
+			return rand() % 3 + 1
+		}else {
+			return rand() % 4
+		}
 	}
 }
