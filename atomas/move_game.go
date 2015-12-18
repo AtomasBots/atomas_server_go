b8c039f3f20b47903a087641a2ee476be6f7cc76
ordon <ordon1313@gmail.com>
2015-12-18 19:11:12 +0100

    end game code

--- atomas/move_game.go
+++ atomas/move_game.go
@@ -6,27 +6,29 @@ import (
 	"strconv"
 )
 
+const END_OF_GAME = -1000
+
 func CreateMoveHandler(games map[string]GameDTO, randomElement func(int) int) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := strings.Split(r.URL.Path, "/")
 		moveTo := path[len(path) - 1]
 		gameId := path[len(path) - 2]
 		game := games[gameId]
-		if (game.Id == gameId) {
+		if (game.Id != gameId) {
+			http.NotFound(w, r)
+		} else {
 			moveToInt, err := strconv.Atoi(moveTo)
 			if (err != nil) {
 				http.Error(w, "Bad request", 502)
-			}else {
-				if (game.Next == nil) {
+			} else {
+				if (game.Next == END_OF_GAME) {
 					http.Error(w, "Bad request", 502)
-				}else {
+				} else {
 					afterMove := Move(game, moveToInt, randomElement(game.Round))
 					games[gameId] = afterMove
 					fmt.Fprint(w, ToJsonString(afterMove))
 				}
 			}
-		}else {
-			http.NotFound(w, r)
 		}
 	}
 }
@@ -35,7 +37,7 @@ func Move(game GameDTO, moveTo int, next int) GameDTO {
 	newBoard := append(game.Board[:moveTo], append([]int{game.Next}, game.Board[moveTo:]...)...)
 	scoreForMove, newBoard := EvaluateBoard(newBoard)
 	if (len(newBoard) > 18) {
-		next = nil
+		next = END_OF_GAME
 	}
 	return GameDTO{
 		Id:game.Id,
