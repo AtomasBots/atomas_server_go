d3ae6e82d6662e9e28e22d0cfd1deaa742f09619
ordon <ordon1313@gmail.com>
2015-12-18 19:25:03 +0100

    refactor

--- atomas/move_game.go
+++ atomas/move_game.go
@@ -4,33 +4,52 @@ import (
 	"strings"
 	"fmt"
 	"strconv"
+	"errors"
 )
 
 const END_OF_GAME = -1000
 
 func CreateMoveHandler(games map[string]GameDTO, randomElement func(int) int) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		path := strings.Split(r.URL.Path, "/")
-		moveTo := path[len(path) - 1]
-		gameId := path[len(path) - 2]
-		game := games[gameId]
-		if (game.Id != gameId) {
-			http.NotFound(w, r)
+		handleMove(w, r, games, randomElement)
+	}
+}
+
+func handleMove(w http.ResponseWriter, r *http.Request, games map[string]GameDTO, randomElement func(int) int) {
+	game, err := findGame(r, games)
+	if (err) {
+		http.Error(w, err.Error(), 404)
+	} else {
+		moveTo, err := extractMoveTo(r)
+		if (err != nil) {
+			http.Error(w, err.Error(), 502)
 		} else {
-			moveToInt, err := strconv.Atoi(moveTo)
-			if (err != nil) {
-				http.Error(w, "Bad request", 502)
+			if (game.Next == END_OF_GAME) {
+				http.Error(w, "Game over", 502)
 			} else {
-				if (game.Next == END_OF_GAME) {
-					http.Error(w, "Bad request", 502)
-				} else {
-					afterMove := Move(game, moveToInt, randomElement(game.Round))
-					games[gameId] = afterMove
-					fmt.Fprint(w, ToJsonString(afterMove))
-				}
+				afterMove := Move(game, moveTo, randomElement(game.Round))
+				games[game.Id] = afterMove
+				fmt.Fprint(w, ToJsonString(afterMove))
 			}
 		}
 	}
+}
+
+func findGame(r *http.Request, games map[string]GameDTO) (*GameDTO, error) {
+	path := strings.Split(r.URL.Path, "/")
+	gameId := path[len(path) - 2]
+	game := games[gameId]
+	if (game.Id != gameId) {
+		return nil, errors.New("Game does not exist")
+	}else {
+		return &game, nil
+	}
+}
+
+func extractMoveTo(r *http.Request) (i int, err error) {
+	path := strings.Split(r.URL.Path, "/")
+	moveTo := path[len(path) - 1]
+	return strconv.Atoi(moveTo)
 }
 
 func Move(game GameDTO, moveTo int, next int) GameDTO {
