27e35f55daa7e320a2768f0ab8e07807275c662a
ordon <ordon1313@gmail.com>
2016-01-03 00:48:06 +0100

    added name param when creating game #7

--- atomas/create_game.go
+++ atomas/create_game.go
@@ -8,16 +8,17 @@ import (
 func CreateCreateGameHandler(games map[string]GameDTO, nextUUID func() string, randomElement func(int) int) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		uuid := nextUUID()
-		newGame := NewGame(uuid, r.RemoteAddr, randomElement)
+		newGame := NewGame(uuid, r.RemoteAddr, r.FormValue("name"), randomElement)
 		games[uuid] = newGame
 		fmt.Fprint(w, ToJsonString(newGame))
 	}
 }
 
-func NewGame(uuid string, ipAddress string, randomElement func(int) int) GameDTO {
+func NewGame(uuid string, ipAddress string, name string, randomElement func(int) int) GameDTO {
 	return GameDTO{
 		Id: uuid,
 		Ip: ipAddress,
+		Name: name,
 		Board: []int{randomElement(0), randomElement(0), randomElement(0), randomElement(0), randomElement(0), randomElement(0)},
 		Next: randomElement(0),
 		Round: 0,
--- atomas/dto.go
+++ atomas/dto.go
@@ -3,6 +3,7 @@ package atomas
 type GameDTO struct {
 	Id    string `json:"id"`
 	Ip    string `json:"ip"`
+	Name    string `json:"name,omitempty"`
 	Board []int `json:"board"`
 	Next  int `json:"next"`
 	Round int `json:"round"`
